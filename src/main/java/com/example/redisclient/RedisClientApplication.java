a0b935a75eadda26c8e489ac17f947fab4e131b9
mojtaba rouhi <roohi.m.1364@gmail.com>
2023-01-01 14:53:55 +0330

    first commit

---
+++ src/main/java/com/example/redisclient/RedisClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.redisclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisClientApplication.class, args);
+    }
+
+}
