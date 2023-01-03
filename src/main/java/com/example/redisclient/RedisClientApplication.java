28031a2a5d851d466eeafd5370c0f58ba7135140
mojtaba rouhi <roohi.m.1364@gmail.com>
2023-01-03 13:12:24 +0330

    fix bug

--- src/main/java/com/example/redisclient/RedisClientApplication.java
+++ src/main/java/com/example/redisclient/RedisClientApplication.java
@@ -2,27 +2,28 @@ package com.example.redisclient;
 
 
 import org.redisson.api.RedissonClient;
-import org.redisson.spring.starter.RedissonAutoConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {RedissonAutoConfiguration.class})
+@EnableAutoConfiguration
 public class RedisClientApplication {
 
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(RedisClientApplication.class, args);
 
-        RedissonClient redis = context.getBean(RedissonClient.class);
-        System.out.print("redis keys Count: " + redis.getKeys().count());
+        try{
+            RedissonClient redis = context.getBean(RedissonClient.class);
+            System.out.print("redis keys Count: " + redis.getKeys().count());
+        } catch (NoSuchBeanDefinitionException ex){
+            System.out.println("not connect to redisson ");
+        }
+
     }
 
 }
--- src/main/java/com/example/redisclient/config/RedissonConfig.java
+++ src/main/java/com/example/redisclient/config/RedissonConfig.java
@@ -3,12 +3,10 @@ package com.example.redisclient.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class RedissonConfig {
 
 
