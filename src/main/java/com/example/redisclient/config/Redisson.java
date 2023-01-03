36adde5da455f269e915fa75e4c4eaddcb175fec
mojtaba rouhi <roohi.m.1364@gmail.com>
2023-01-03 17:10:20 +0330

    change type to method connection properties

---
+++ src/main/java/com/example/redisclient/config/Redisson.java
@@ -0,0 +1,34 @@
+package com.example.redisclient.config;
+
+import com.example.redisclient.model.RedissonProperty;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class Redisson {
+
+    @Bean
+    public RedissonClient Redisson(RedissonProperty redissonProperty){
+        Config config=new Config();
+        System.out.println("redis-client-connect");
+        if(redissonProperty.isCluster()){
+             config.useClusterServers()
+                     .setPassword(redissonProperty.getPassword());
+//                     .setNodeAddresses();
+        } else{
+            config.useSingleServer().setAddress(redissonProperty.getAddress());
+            if( redissonProperty.getPassword()!=null)
+                config.useSingleServer().setPassword (redissonProperty.getPassword());
+        }
+        return org.redisson.Redisson.create(config);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "scores-redis")
+    public RedissonProperty redissonProperty() {
+        return new RedissonProperty();
+    }
+}
---
+++ src/main/java/com/example/redisclient/model/RedissonProperty.java
@@ -0,0 +1,47 @@
+package com.example.redisclient.model;
+
+
+public class RedissonProperty {
+
+    private String address;
+
+    private String password;
+
+    private boolean cluster;
+
+    private String prefix;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isCluster() {
+        return cluster;
+    }
+
+    public void setCluster(boolean cluster) {
+        this.cluster = cluster;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+}
+
