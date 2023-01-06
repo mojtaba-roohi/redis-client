724e9c4e22c1b778368c6acfab435feaaca54a8d
mojtaba rouhi <exir.m.roohi@gmail.com>
2023-01-07 00:23:07 +0330

    add redis score service

--- src/main/java/com/example/redisclient/RedisClientApplication.java
+++ src/main/java/com/example/redisclient/RedisClientApplication.java
@@ -1,8 +1,10 @@
 package com.example.redisclient;
 
 
+import org.rajman.common.scores.service.ScoreService;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,13 +12,19 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class RedisClientApplication {
 
+  static  ScoreService scoreService;
+
+    public RedisClientApplication(ScoreService scoreService) {
+        this.scoreService = scoreService;
+    }
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(RedisClientApplication.class, args);
 
+        Integer m= scoreService.getScore("abo");
+        System.out.println(m);
         try{
             RedissonClient redis = context.getBean(RedissonClient.class);
             System.out.print("redis keys Count: " + redis.getKeys().count());
